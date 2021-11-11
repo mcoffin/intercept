9933307a94cf379081bb5e905579c4ce08343d16
Matt Coffin <mcoffin13@gmail.com>
2021-11-11 14:18:03 -0700

    spdlog: bring back tweakme from original intercept

--- src/host/common/system/spdlog/tweakme.h
+++ src/host/common/system/spdlog/tweakme.h
@@ -17,13 +17,13 @@
 // This clock is less accurate - can be off by dozens of millis - depending on the kernel HZ.
 // Uncomment to use it instead of the regular clock.
 //
-#define SPDLOG_CLOCK_COARSE
+// #define SPDLOG_CLOCK_COARSE
 ///////////////////////////////////////////////////////////////////////////////
 
 
 ///////////////////////////////////////////////////////////////////////////////
 // Uncomment if date/time logging is not needed and never appear in the log pattern.
-// This will prevent spdlog from quering the clock on each log call.
+// This will prevent spdlog from querying the clock on each log call.
 //
 // WARNING: If the log pattern contains any date/time while this flag is on, the result is undefined.
 //          You must set new pattern(spdlog::set_pattern(..") without any date/time in it
@@ -34,7 +34,7 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 // Uncomment if thread id logging is not needed (i.e. no %t in the log pattern).
-// This will prevent spdlog from quering the thread id on each log call.
+// This will prevent spdlog from querying the thread id on each log call.
 //
 // WARNING: If the log pattern contains thread id (i.e, %t) while this flag is on, the result is undefined.
 //
@@ -43,10 +43,20 @@
 
 
 ///////////////////////////////////////////////////////////////////////////////
+// Uncomment to prevent spdlog from caching thread ids in thread local storage.
+// By default spdlog saves thread ids in tls to gain a few micros for each call.
+//
+// WARNING: if your program forks, UNCOMMENT this flag to prevent undefined thread ids in the children logs.
+//
+// #define SPDLOG_DISABLE_TID_CACHING
+///////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
 // Uncomment if logger name logging is not needed.
 // This will prevent spdlog from copying the logger name  on each log call.
 //
-#define SPDLOG_NO_NAME
+// #define SPDLOG_NO_NAME
 ///////////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -59,10 +69,10 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 // Uncomment to avoid locking in the registry operations (spdlog::get(), spdlog::drop() spdlog::register()).
-// Use only if your code never modifes concurrently the registry.
+// Use only if your code never modifies concurrently the registry.
 // Note that upon creating a logger the registry is modified by spdlog..
 //
-//#define SPDLOG_NO_REGISTRY_MUTEX
+// #define SPDLOG_NO_REGISTRY_MUTEX
 ///////////////////////////////////////////////////////////////////////////////
 
 
@@ -70,14 +80,16 @@
 // Uncomment to avoid spdlog's usage of atomic log levels
 // Use only if your code never modifies a logger's log levels concurrently by different threads.
 //
-#define SPDLOG_NO_ATOMIC_LEVELS
+// #define SPDLOG_NO_ATOMIC_LEVELS
 ///////////////////////////////////////////////////////////////////////////////
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // Uncomment to enable usage of wchar_t for file names on Windows.
 //
 // #define SPDLOG_WCHAR_FILENAMES
 ///////////////////////////////////////////////////////////////////////////////
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // Uncomment to override default eol ("\n" or "\r\n" under Linux/Windows)
@@ -95,6 +107,14 @@
 
 
 ///////////////////////////////////////////////////////////////////////////////
+// Uncomment to use printf-style messages in your logs instead of the usual
+// format-style used by default.
+//
+// #define SPDLOG_FMT_PRINTF
+///////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
 // Uncomment to enable syslog (disabled by default)
 //
 // #define SPDLOG_ENABLE_SYSLOG
@@ -102,7 +122,39 @@
 
 
 ///////////////////////////////////////////////////////////////////////////////
+// Uncomment to enable wchar_t support (convert to utf8)
+//
+// #define SPDLOG_WCHAR_TO_UTF8_SUPPORT
+///////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
 // Uncomment to prevent child processes from inheriting log file descriptors
 //
 // #define SPDLOG_PREVENT_CHILD_FD
+///////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Uncomment if your compiler doesn't support the "final" keyword.
+// The final keyword allows more optimizations in release
+// mode with recent compilers. See GCC's documentation for -Wsuggest-final-types
+// for instance.
+//
+// #define SPDLOG_NO_FINAL
+///////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Uncomment to enable message counting feature.
+// Use the %i in the logger pattern to display log message sequence id.
+//
+// #define SPDLOG_ENABLE_MESSAGE_COUNTER
+///////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Uncomment to customize level names (e.g. "MT TRACE")
+//
+// #define SPDLOG_LEVEL_NAMES  { "MY TRACE", "MY DEBUG", "MY INFO", "MY WARNING", "MY ERROR", "MY CRITICAL", "OFF" }
 ///////////////////////////////////////////////////////////////////////////////
